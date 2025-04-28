3cf230d009a9e8acc77e2a985d0a69d15ac3d0a3
CodeNoCode888 <waltjr7777@gmail.com>
2025-04-28 11:18:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Initialize AOS library
-AOS.init({
-  duration: 1000,
-  once: true,
+
+// Smooth Scroll on load
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('section');
+
+  const options = {
+    threshold: 0.5
+  };
+
+  const observer = new IntersectionObserver(function(entries, observer){
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('appear');
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
 });
